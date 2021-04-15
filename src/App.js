795e28a48e50b87564915c4eeb853abfbd557b72
James Ohsako <johsako087@gmail.com>
2021-04-14 21:22:49 -0700

    removed all trace of react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Home from './pages';
-import SignIn from './pages/signin'
+import React, {useState} from 'react'
+import HeroSection from '../components/HeroSection'
+import InfoSection from '../components/InfoSection'
+import Projects from '../components/Projects'
+import {contact, homeObjOne, homeObjThree, homeObjTwo } from '../components/InfoSection/Data'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
+// import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+// import Home from './pages';
+// import SignIn from './pages/signin'
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path='/' component={Home} exact />
-        <Route path='/signin' component={SignIn} exact />
-      </Switch>
-    </Router>
+    <>
+    <Sidebar isOpen={isOpen} toggle={toggle} /> 
+    <Navbar toggle={toggle} />
+    <HeroSection />
+    <InfoSection {...homeObjOne} />
+    <InfoSection {...homeObjTwo} />
+    <InfoSection {...homeObjThree} />
+    <Projects />
+    <InfoSection {...contact} />
+    <Footer />
+    </>
   );
 }
 
